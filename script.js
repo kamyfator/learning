26ba4129313b3a89950f28c1359c67d21c829894
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2018-09-26 20:44:51 +0200

    Dziedziczenie

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 var someAnimal;
+var someDog;
 
 class animal{
     constructor(name, sound){
         this.name = name;
         this.sound = sound;
+    }
+}
+
+class dog extends animal{
+    constructor(color, name){
+        super(name, "woof");
+        this.color = color;
     }
 }
 
@@ -13,12 +21,19 @@ function createAnimal(name, sound){
     someAnimal.sound = sound;
 }
 
+function createDog(name, color){
+    someDog = new dog(color, name);
+    someDog.name = name;
+}
+
 function makeSmaller(animal){
     animal.name = "little " + animal.name;
     animal.sound = "cute " + animal.sound;
     return animal;
 }
 
-function show(){
-    document.getElementById('paragraph').innerHTML = "My name is " + someAnimal.name + " and i do " + someAnimal.sound;
+
+function show(animal){
+    document.getElementById('paragraph').innerHTML = "My name is " + animal.name + " and i do " + animal.sound +
+    " and my color is " + animal.color;
 }
