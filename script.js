3c03e87dba4099d7a454fabde3c25479a6391439
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2018-09-27 19:51:59 +0200

    Tamplate pod testowanie rozwiązań

--- script.js
+++ script.js
@@ -1,39 +1,10 @@
-var someAnimal;
-var someDog;
+var dane = "dane";
 
-class animal{
-    constructor(name, sound){
-        this.name = name;
-        this.sound = sound;
-    }
+function pokazDane(){
+    document.getElementById("dane").innerHTML = dane;
 }
 
-class dog extends animal{
-    constructor(color, name){
-        super(name, "woof");
-        this.color = color;
-    }
+function wykonaj(){
+    document.getElementById("rezultat").innerHTML = "rezultat";
 }
 
-function createAnimal(name, sound){
-    someAnimal = new animal();
-    someAnimal.name = name;
-    someAnimal.sound = sound;
-}
-
-function createDog(name, color){
-    someDog = new dog(color, name);
-    someDog.name = name;
-}
-
-function makeSmaller(animal){
-    animal.name = "little " + animal.name;
-    animal.sound = "cute " + animal.sound;
-    return animal;
-}
-
-
-function show(animal){
-    document.getElementById('paragraph').innerHTML = "My name is " + animal.name + " and i do " + animal.sound +
-    " and my color is " + animal.color;
-}
