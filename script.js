383ad83ffdee781a91824f5a3220c342456afffa
kamyfator <kamila.sternicka@gmail.com>
2018-09-27 20:33:06 +0200

    non unique

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-var dane = "dane";
+var dane = [1,2,1,3,2,5,1];
 
 function pokazDane(){
     document.getElementById("dane").innerHTML = dane;
 }
 
 function wykonaj(){
-    document.getElementById("rezultat").innerHTML = "rezultat";
+    document.getElementById("rezultat").innerHTML = funkcja(dane);
+}
+
+function funkcja(dane){
+    var tablica = [];
+    for (let item of dane){
+        if (!isUnique(dane, item)){
+            tablica.push(item);
+        }
+    }
+    return tablica;
+}
+
+function isUnique(arr, el) {
+    var hasOne = false;
+    for (let item of arr) {
+        if (item === el) {
+            if (hasOne) {
+                return false;
+            } else {
+                hasOne = true;
+            }
+        }
+    }
+    return true;
 }
 
