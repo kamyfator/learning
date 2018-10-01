13700f8564548373c7f1bccad3f92f3d019feeab
kamyfator <kamila.sternicka@gmail.com>
2018-10-01 20:34:10 +0200

    say Hi

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var dane = 'azazazaz';
+var name;
+var age;
 
 function pokazDane() {
     document.getElementById("dane").innerHTML = dane;
@@ -8,25 +9,6 @@ function wykonaj() {
     document.getElementById("rezultat").innerHTML = licznik(dane);
 }
 
-function licznik(data) {
-    var max = 1;
-    var i = 0;
-    var lastLetter = data[0];
-    var count = 1;
-    if (data == '') {
-        return 0;
-    }
-    for (i = 1; i < data.length; i++) {
-        if (data[i] == lastLetter) {
-            count++;
-            if (count > max) {
-                max = count;
-            }
-        }
-        else {
-            count = 1;
-        }
-        lastLetter = data[i];
-    }
-    return max;
+function sayHi(name, age) {
+    return "Hi. My name is " + name + " and I'm " + age + " years old.";
 }
