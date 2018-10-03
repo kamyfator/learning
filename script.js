ec74ed1e71effcc0a9465d2d1c1e5693b7d0a3f4
kamyfator <kamila.sternicka@gmail.com>
2018-10-03 21:04:44 +0200

    index power

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
-var dane = '00:00';
-
+var dane = [0,0];
+var n=3;
 
 function pokazDane() {
-    document.getElementById("dane").innerHTML = dane;
+    document.getElementById("dane").innerHTML = dane + '   ' + n;
 }
 
 function wykonaj() {
-    document.getElementById("rezultat").innerHTML = timeConverter(dane);
+    document.getElementById("rezultat").innerHTML = index(dane, n);
 }
 
-function timeConverter(dane) {
-    var postFormat;
-    if (dane[0] * 10 + dane[1] * 1 < 12) {
-        if(dane[0] * 1 + dane[1] * 1 == 0) {
-            postFormat = '12:00';
-        }
-        else if (dane[0] == '0') {
-            postFormat = dane.slice(1,5);
-        }
-        else {
-            postFormat = dane;
-        }
-        return postFormat + ' a.m.'
+function index(dane, n) {
+    var liczba;
+    if(n >= dane.length){
+        return -1;
     }
     else {
-        if (dane[0] * 10 + dane[1] * 1 == 12) {
-            postFormat = dane;
-        }
-        else {
-            var hour = dane[0] * 10 + dane[1] * 1 - 12;
-            postFormat = hour + ':' + dane.slice(3);
-        } 
+        liczba = Math.pow (dane[n], n);
+        return liczba;    
     }
-    return postFormat + ' p.m.';
 }
