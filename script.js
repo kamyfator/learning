1757d43311b77d300366439f73f0a15309af4c1e
kamyfator <kamila.sternicka@gmail.com>
2018-10-03 21:12:38 +0200

    difference (checkio error)

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-var dane = [0,0];
-var n=3;
+var dane = [1,9,-6,4];
 
 function pokazDane() {
-    document.getElementById("dane").innerHTML = dane + '   ' + n;
+    document.getElementById("dane").innerHTML = dane;
 }
 
 function wykonaj() {
-    document.getElementById("rezultat").innerHTML = index(dane, n);
+    document.getElementById("rezultat").innerHTML = difference(dane);
 }
 
-function index(dane, n) {
-    var liczba;
-    if(n >= dane.length){
-        return -1;
-    }
-    else {
-        liczba = Math.pow (dane[n], n);
-        return liczba;    
-    }
+function difference(dane) {
+    return Math.max.apply(null, dane) - Math.min.apply(null,dane);
 }
