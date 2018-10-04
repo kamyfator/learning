291ee6805eb052e714112dd7ebfb79d5a959ec4d
kamyfator <kamila.sternicka@gmail.com>
2018-10-04 20:57:32 +0200

    bubble sort

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-var dane = [1,9,-6,4];
+var dane = [1,2,3,4];
 
 function pokazDane() {
     document.getElementById("dane").innerHTML = dane;
 }
 
 function wykonaj() {
-    document.getElementById("rezultat").innerHTML = difference(dane);
+    document.getElementById("rezultat").innerHTML = bubble(dane);
+}
+// Think twice, code once
+function swap(dane, a, b) {
+    var z;
+    z = dane[a];
+    dane[a] = dane[b];
+    dane[b] = z;
 }
 
-function difference(dane) {
-    return Math.max.apply(null, dane) - Math.min.apply(null,dane);
+function bubble(dane) {
+    var i = 0;
+    var j = 0;
+    var appear = true;
+    for (j = 0; j < dane.length; j++) {
+        appear = false;
+        for (i = 0; i < dane.length; i++) {
+            if (dane[i] > dane[i + 1]) {
+                swap(dane, i, i + 1);
+                console.log(dane);
+                appear = true;
+            }
+        }
+        if (!appear) break;
+    }
+    return dane;
 }
