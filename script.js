a3d2cf44558bff6545d1aad2500b60fd6b06da55
kamyfator <kamila.sternicka@gmail.com>
2018-10-04 21:52:33 +0200

    selection sort

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
-var dane = [1,2,3,4];
+var dane = [1, 4, 3, 6, 0, -2, -9, 7, 1];
 
 function pokazDane() {
     document.getElementById("dane").innerHTML = dane;
 }
 
 function wykonaj() {
-    document.getElementById("rezultat").innerHTML = bubble(dane);
+    document.getElementById("rezultat").innerHTML = selection(dane);
 }
 // Think twice, code once
-function swap(dane, a, b) {
-    var z;
-    z = dane[a];
-    dane[a] = dane[b];
-    dane[b] = z;
-}
 
-function bubble(dane) {
-    var i = 0;
-    var j = 0;
-    var appear = true;
-    for (j = 0; j < dane.length; j++) {
-        appear = false;
-        for (i = 0; i < dane.length; i++) {
-            if (dane[i] > dane[i + 1]) {
-                swap(dane, i, i + 1);
-                console.log(dane);
-                appear = true;
-            }
-        }
-        if (!appear) break;
+function selection(dane) {
+    var index;
+    var rezultat = [];
+    for (; dane.length != 0;) {
+        rezultat.push(Math.min.apply(null, dane));
+        console.log(rezultat);
+        index = dane.indexOf(Math.min.apply(null, dane));
+        dane.splice(index, 1);
+        console.log(dane);
     }
-    return dane;
+    return rezultat;
 }
