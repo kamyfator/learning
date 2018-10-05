669a0f41b19eef0ccc5f391ca53b499d1d6077e2
kamyfator <kamila.sternicka@gmail.com>
2018-10-05 18:17:10 +0200

    xoxo

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-var dane = [1, 4, 3, 6, 0, -2, -9, 7, 1];
+var dane = (["OO.",
+            "XOX",
+            "XOX"]);
 
 function pokazDane() {
     document.getElementById("dane").innerHTML = dane;
 }
 
 function wykonaj() {
-    document.getElementById("rezultat").innerHTML = selection(dane);
+    document.getElementById("rezultat").innerHTML = xo(dane);
 }
 // Think twice, code once
 
-function selection(dane) {
-    var index;
-    var rezultat = [];
-    for (; dane.length != 0;) {
-        rezultat.push(Math.min.apply(null, dane));
-        console.log(rezultat);
-        index = dane.indexOf(Math.min.apply(null, dane));
-        dane.splice(index, 1);
-        console.log(dane);
+function xo(dane) {
+    var j = 0;
+    for (; j < 3; j++) {
+        if (dane[0][j] == 'X' && dane[1][j] == 'X' && dane[2][j] == 'X') {
+            return 'X';
+        }
+        else if (dane[0][j] == 'O' && dane[1][j] == 'O' && dane[2][j] == 'O') {
+            return 'O';
+        }
+        else if (dane[j][0] == 'X' && dane[j][1] == 'X' && dane[j][2] == 'X') {
+            return 'X';
+        }
+        else if (dane[j][0] == 'O' && dane[j][1] == 'O' && dane[j][2] == 'O') {
+            return 'O';
+        }
+        else if (dane[0][0] == 'X' && dane[1][1] == 'X' && dane[2][2] == 'X') {
+            return 'X';
+        }
+        else if (dane[0][2] == 'X' && dane[1][1] == 'X' && dane[2][0] == 'X') {
+            return 'X';
+        }
+        else if (dane[0][0] == 'O' && dane[1][1] == 'O' && dane[2][2] == 'O') {
+            return 'O';
+        }
+        else if (dane[0][2] == 'O' && dane[1][1] == 'O' && dane[2][0] == 'O') {
+            return 'O';
+        }
     }
-    return rezultat;
+    return 'D';
 }
