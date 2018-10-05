e3112af1946744ea38334457d5e65ea6e9b922c4
kamyfator <kamila.sternicka@gmail.com>
2018-10-05 18:18:51 +0200

    even the last (addition and multiplication)

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
-var dane = (["OO.",
-            "XOX",
-            "XOX"]);
+var dane = [1,5,2,7];
 
 function pokazDane() {
     document.getElementById("dane").innerHTML = dane;
 }
 
 function wykonaj() {
-    document.getElementById("rezultat").innerHTML = xo(dane);
+    document.getElementById("rezultat").innerHTML = multiply(dane);
 }
 // Think twice, code once
 
-function xo(dane) {
-    var j = 0;
-    for (; j < 3; j++) {
-        if (dane[0][j] == 'X' && dane[1][j] == 'X' && dane[2][j] == 'X') {
-            return 'X';
-        }
-        else if (dane[0][j] == 'O' && dane[1][j] == 'O' && dane[2][j] == 'O') {
-            return 'O';
-        }
-        else if (dane[j][0] == 'X' && dane[j][1] == 'X' && dane[j][2] == 'X') {
-            return 'X';
-        }
-        else if (dane[j][0] == 'O' && dane[j][1] == 'O' && dane[j][2] == 'O') {
-            return 'O';
-        }
-        else if (dane[0][0] == 'X' && dane[1][1] == 'X' && dane[2][2] == 'X') {
-            return 'X';
-        }
-        else if (dane[0][2] == 'X' && dane[1][1] == 'X' && dane[2][0] == 'X') {
-            return 'X';
-        }
-        else if (dane[0][0] == 'O' && dane[1][1] == 'O' && dane[2][2] == 'O') {
-            return 'O';
-        }
-        else if (dane[0][2] == 'O' && dane[1][1] == 'O' && dane[2][0] == 'O') {
-            return 'O';
+function multiply(dane) {
+    var suma=0;
+    var i=0;
+    if (dane.length == 0) return 0;
+    for ( ; i < dane.length; i++) {
+        //index = dane.indexOf(i);
+        console.log(dane[i]);
+        if (i % 2 == 0) {
+            suma=suma+dane[i];
         }
     }
-    return 'D';
+    return suma * dane[dane.length-1];
 }
